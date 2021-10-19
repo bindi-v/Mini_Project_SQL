45575c779bbcf1cde580e68a9f27c37372fc824b
bindi-v <bindi.vaghela@gmail.com>
2021-10-19 10:51:55 -0400

    create table and movie db

---
+++ Main/db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO movies (movie_name)
+VALUES ("JAb We Met"),
+        ("Lion King"),
+       ("The Godfather"),
+       ("Dhoom3"),
+       ("Bahubali"),
+       ("West Side Story"),
+       ("Parasite"),
+       ("The Wizard of Oz"),
+       ("Gujjubhai the Great")
+       ;
+
+INSERT INTO reviews (movie_id, review)
+VALUES (2, "Zazu is underrated. Give that hornbill a sequel!"),
+       (3, "I'm gonna make him an offer you can't refuse, watch this movie"),
+       (2, "Scar is the lion everyone loves to hate"),
+       (6, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
+       (8, "The tin man gave a metallic, hollow performance"),
+       (2, "Hakuna matata"),
+       (8, "Those flying monkeys are nightmare fuel!"),
+       (5, "Amazing story!"),
+       (9, "What a fun movie!");
